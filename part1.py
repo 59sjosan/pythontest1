c11bb3b19ea1e258fc94a7f7430d15e6b17a94f3
59sjosan <josansejal2002@gmail.com>
2023-04-04 11:51:34 -0400

    v1

--- part1.py
+++ part1.py
@@ -1,12 +1,12 @@
 month=int(input("Enter the month: "))
 day=int(input("Enter the day of the month: "))
 year=int(input("Enter the year in two digit format: "))
-if(month>12):
-  print("Error:Invalid Month Input")
-elif(day>31):
-  print( "Error: Invalid Day Input")
-elif(year>20):
-  print("Error: Invalid Year Input")
+if(month>12 or month<0):
+   print("Error: Invalid Month Input")
+elif(day>31 or day<0):
+  print("Error: Invalid day Input")
+elif(year>99 or year<0):  
+   print("Error: Invalid year Input")
 else:
-  print(month,"/", day,"/", year)
-  print("Success: Congratulations! you entered the correct date.")
+  print("successful you  have entered correct date ")
+  print("date:",month,"/",day,"/",year)
